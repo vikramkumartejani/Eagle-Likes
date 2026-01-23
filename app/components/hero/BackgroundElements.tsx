ab2af331755244c85c991e3e0ab3b3b8c9c48d99
vikramkumartejani <vikramkumar8t8@gmail.com>
2026-01-24 03:13:15 +0500

    update

--- app/components/hero/BackgroundElements.tsx
+++ app/components/hero/BackgroundElements.tsx
@@ -3,44 +3,53 @@ import Image from 'next/image';
 
 export const BackgroundGrid = () => (
     <div className="absolute inset-0 z-20 w-full mx-auto mt-12.75 md:mt-24 pointer-events-none -ml-[5px]">
-        <Image src="/assets/grid.svg" alt="Background Grid" width={762} height={365} className='md:block hidden object-contain mx-auto' />
-        <Image src="/assets/svg-grid-mb.svg" alt="Background Grid" width={374} height={221} className='md:hidden block object-contain mx-auto' />
+        <Image src="/assets/grid.svg" alt="Background Grid" width={762} height={365} loading="lazy" unoptimized className='md:block hidden object-contain mx-auto' />
+        <Image src="/assets/svg-grid-mb.svg" alt="Background Grid" width={374} height={221} loading="lazy" unoptimized className='md:hidden block object-contain mx-auto' />
     </div>
 );
 
 export const HeroRightImage = () => (
-    <Image src="/assets/right-shadow-hero.png" alt="Background Grid" unoptimized width={460} height={447} className='absolute -right-62 top-6 z-10 md:block hidden opacity-30' />
+    <Image src="/assets/right-shadow-hero.png" alt="Background Grid" loading="lazy" unoptimized width={460} height={447} className='absolute -right-62 top-6 z-10 md:block hidden opacity-30' />
 );
 
 export const HeroLeftImage = () => (
-    <Image src="/assets/left-shadow-hero.svg" alt="Background Grid" unoptimized width={213} height={447} className='absolute left-0 top-80 md:block hidden' />
+    <Image src="/assets/left-shadow-hero.svg" alt="Background Grid" loading="lazy" unoptimized width={213} height={447} className='absolute left-0 top-80 md:block hidden' />
 );
 
 export const LeftShadow = () => (
-    <div
-        className="hidden md:block absolute -z-10 -left-[143px] top-[10%] lg:-left-[400px] lg:top-[280px] w-[179px] h-[457px] lg:w-[579px] lg:h-[579px] shrink-0 rounded-[457px] lg:rounded-[579px]"
-        style={{
-            background: "rgba(0, 103, 219, 0.80)",
-            filter: "blur(100px)",
-            WebkitFilter: "blur(100px)",
-            contain: "paint",
-            transform: "translateZ(0)",
-        }}
-    ></div>
+    <div className="hidden md:block absolute -z-10 top-[4%] left-0">
+        <svg width="427" height="979" viewBox="0 0 427 979" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_1_17)">
+                <circle cx="-62.5" cy="489.5" r="289.5" fill="#0067DB" fillOpacity="0.8" />
+            </g>
+            <defs>
+                <filter id="filter0_f_1_17" x="-552" y="0" width="979" height="979" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_1_17" />
+                </filter>
+            </defs>
+        </svg>
+    </div>
 
 );
 
 export const RightShadow = () => (
-    <div
-        className="hidden md:block absolute -z-10 -right-[123px] top-[10%] lg:-right-[479px] lg:top-[370px] w-[170px] h-[460px] lg:w-[579px] lg:h-[579px] shrink-0 rounded-[460px] lg:rounded-[579px]"
-        style={{
-            background: "rgba(0, 103, 219, 0.80)",
-            filter: "blur(100px)",
-            WebkitFilter: "blur(100px)",
-            contain: "paint",
-            transform: "translateZ(0)",
-        }}
-    ></div>
+    <div className="hidden md:block absolute -z-10 top-[10%] right-0 lg:top-[170px]">
+        <svg width="300" height="979" viewBox="0 0 300 979" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_1_16)">
+                <circle cx="489.5" cy="489.5" r="289.5" fill="#0067DB" fillOpacity="0.8" />
+            </g>
+            <defs>
+                <filter id="filter0_f_1_16" x="0" y="0" width="979" height="979" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_1_16" />
+                </filter>
+            </defs>
+        </svg>
+
+    </div>
 );
 
 export const LeftBottomShadow = () => (
@@ -77,15 +86,17 @@ export const LeftBottomShadow = () => (
 
 export const TopBlueGlow = () => (
     <div className="flex w-full justify-center ">
-        <div
-            className="absolute -top-[100px] lg:-top-[289px] w-[310px] h-[246px] lg:w-[579px] lg:h-[579px] shrink-0 rounded-[460px] lg:rounded-[579px]"
-            style={{
-                background: "rgba(0, 103, 219, 0.80)",
-                filter: "blur(100px)",
-                WebkitFilter: "blur(100px)",
-                contain: "paint",
-                transform: "translateZ(0)",
-            }}
-        ></div>
+        <svg width="979" height="490" className=' absolute top-0' viewBox="0 0 979 490" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_1_15)">
+                <circle cx="489.5" cy="0.5" r="289.5" fill="#0067DB" fillOpacity="0.8" />
+            </g>
+            <defs>
+                <filter id="filter0_f_1_15" x="0" y="-489" width="979" height="979" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_1_15" />
+                </filter>
+            </defs>
+        </svg>
     </div>
 );
