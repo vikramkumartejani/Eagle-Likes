15462c564ec3cf9cf68e17a4cccc254b18c78205
vikramkumartejani <vikramkumar8t8@gmail.com>
2026-03-12 11:58:44 +0500

    p

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
   return (
     <div className="text-white overflow-x-hidden">
       <Header />
-      <Hero />
+      {/* <Hero /> */}
       <EagleLikesIsSeenOn />
       <div>
         <WhyEagleLikes />
